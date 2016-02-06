5b022ae040438c24007a3df4d8bc59d2c6311506
richwingert <richwingert@gmail.com>
2016-02-06 03:52:38 -0500

    f

---
+++ otherCode/WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //label5.Text = list.accountIndex.getUsername();
+            //label6.Text = list.accountIndex.getPoints().ToString();
+            //label7.Text = list.accountIndex.getPhoneNumber().ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 f = new Form2();
+            this.Hide();
+            f.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form3 f = new Form3();
+            this.Hide();
+            f.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Form4 f = new Form4();
+            this.Hide();
+            f.Show();
+        }
+    }
+
+    public class surveyResults
+    {
+
+    }
+}
